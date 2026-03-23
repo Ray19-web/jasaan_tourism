6c3a24764e4e6cf57fddae0e978faa768491628f
Raymund Gorres <122578070+Ray19-web@users.noreply.github.com>
2026-03-23 19:15:41 +0800

    Delete frontend directory

--- frontend/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <div class="footer-content">
-        <img src="../../assets/images/branding.png" alt="JASAYA Logo" class="footer-logo">
-        <p class="footer-copy">
-            © 2026 JASAAN TOURISM INFORMATION MANAGEMENT SYSTEM <br>
-            BY TEAM YEY!
-        </p>
-    </div>
-</footer>
