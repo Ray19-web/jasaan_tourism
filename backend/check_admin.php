9864827003e5c05e916e7c0d4ebbc26ca308620e
Raymund Gorres <122578070+Ray19-web@users.noreply.github.com>
2026-03-23 19:15:25 +0800

    Delete backend directory

--- backend/check_admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "check_session.php";
-
-if (!isset($_SESSION['role']) || strtolower($_SESSION['role']) !== 'admin') {
-    header("Location: /jasaan-tourism/frontend/pages/user/user_explore.php");
-    exit;
-}
--- backend/check_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['user_id'])) {
-    // Don't force redirect to protected pages
-    header("Location: /jasaan-tourism/frontend/pages/user/user_explore.php");
-    exit;
-}
